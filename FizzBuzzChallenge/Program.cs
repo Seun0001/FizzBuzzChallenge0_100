e8a721bd75ed8b4844004041fecf9487a732e3ad
Seun0001 <oluwaseunafolabi941@gmail.com>
2025-10-07 11:23:22 +0100

    Added FizzBuzz Challenge in C#

---
+++ FizzBuzzChallenge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string x = "Fizz";
+        string y = "Buzz";
+        string z = "FizzBuzz";
+
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+                Console.WriteLine($"{i} - {z}");
+            else if (i % 3 == 0)
+                Console.WriteLine($"{i} - {x}");
+            else if (i % 5 == 0)
+                Console.WriteLine($"{i} - {y}");
+            else
+                Console.WriteLine(i);
+        }
+        Console.WriteLine("Finishing My FizzBuzz Code in 3mins...");
+    }
+}
